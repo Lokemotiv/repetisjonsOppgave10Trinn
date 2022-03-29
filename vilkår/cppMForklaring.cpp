9cca088b6b1cc6287181a015dc665f5f79ebfd59
Lokemotiv <lokesh@live.no>
2022-03-29 09:12:57 +0200

    cppMForklaring fil laget for vilkår

---
+++ vilkår/cppMForklaring.cpp
@@ -0,0 +1,16 @@
+//var := variabel
+
+int main(){
+    //setter var a til svaret på vilkåret: er 1 lik 2.
+    bool a = 1 == 2;
+    //setter var b til svaret på vilkåret: er 1 ikke lik 2.
+    bool b = 1 != 2;
+    //setter var c til svaret på vilkåret: er 1 støre eller lik 2.
+    bool c = 1 >= 2;
+    //setter var d til svaret på vilkåret: er 1 støre enn 2.
+    bool d = 1 > 2;
+    //setter var e til svaret på vilkåret: er 1 mindre eller lik 2.
+    bool e = 1 <= 2;
+    //setter var f til svaret på vilkåret: er 1 mindre enn 2.
+    bool f = 1 < 2;
+}
